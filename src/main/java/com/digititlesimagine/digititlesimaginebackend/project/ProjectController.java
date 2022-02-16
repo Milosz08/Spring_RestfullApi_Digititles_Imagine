747ef628735e6efbc94e7c9635f661e69d46aa34
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-16 23:29:33 +0100

    create project services class (with helper class and interface)

---
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/project/ProjectController.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: ProjectController.java
+ * Last modified: 15/02/2022, 14:38
+ * Project name: digititles-imagine-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package com.digititlesimagine.digititlesimaginebackend.project;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static com.digititlesimagine.digititlesimaginebackend.utils.ServletConfigurer.*;
+
+@RestController
+@RequestMapping(APP_PREFIX + PROJECTS)
+@CrossOrigin
+public class ProjectController {
+
+    @Autowired
+    private ProjectServices services;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectModel>> getAllProjects() {
+        return new ResponseEntity<>(services.getAllProjects(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{idValue}")
+    public ResponseEntity<ProjectModel> getSingleProjectById(@PathVariable String idValue) {
+        return new ResponseEntity<>(services.getSingleProjectById(idValue), HttpStatus.OK);
+    }
+
+    @GetMapping("/title/{titleValue}")
+    public ResponseEntity<ProjectModel> getSingleProjectByTitle(@PathVariable String titleValue) {
+        return new ResponseEntity<>(services.getSingleProjectByTitle(titleValue), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ProjectModel> addSingleProject(@Valid @RequestBody ProjectModel project) {
+        return new ResponseEntity<>(services.addSingleProject(project), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{idValue}")
+    public ResponseEntity<ProjectModel> editSingleProject(
+            @Valid @RequestBody ProjectModel project, @PathVariable String idValue
+    ) {
+        return new ResponseEntity<>(services.editSingleProject(idValue, project), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{idValue}")
+    public ResponseEntity<?> deleteSingleProject(@PathVariable String idValue) {
+        services.deleteSingleProject(idValue);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteAllProjects() {
+        services.deleteAllProjects();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/project/ProjectServicesImplementation.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: ProjectServicesImplementation.java
+ * Last modified: 15/02/2022, 14:37
+ * Project name: digititles-imagine-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package com.digititlesimagine.digititlesimaginebackend.project;
+
+import com.digititlesimagine.digititlesimaginebackend.exceptions.ApiRequestException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class ProjectServicesImplementation extends ServicesImplementationHelpers implements ProjectServices {
+
+    @Override
+    public List<ProjectModel> getAllProjects() {
+        List<ProjectModel> reversed = repository.findAll();
+        Collections.reverse(reversed);
+        for(ProjectModel singleProject : reversed) {
+            sortAboutAndProductionParagraphs(singleProject);
+        }
+        return reversed;
+    }
+
+    @Override
+    public ProjectModel getSingleProjectById(String id) {
+        Optional<ProjectModel> foundProject = repository.findProjectModelById(id);
+        if (foundProject.isPresent()) {
+            ProjectModel sortedProject = foundProject.get();
+            sortAboutAndProductionParagraphs(sortedProject);
+            return sortedProject;
+        }
+        throw new ApiRequestException("Project with id: '" + id + "' does not exist", HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public ProjectModel getSingleProjectByTitle(String title) {
+        Optional<ProjectModel> foundProject = repository.findProjectModelByTitle(title);
+        if (foundProject.isPresent()) {
+            ProjectModel sortedProject = foundProject.get();
+            sortAboutAndProductionParagraphs(sortedProject);
+            return sortedProject;
+        }
+        throw new ApiRequestException("Project with title: '" + title + "' does not exist", HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public ProjectModel addSingleProject(ProjectModel project) {
+        ProjectModel savedData = saveSingleProjectSoftCascade(project);
+        sortAboutAndProductionParagraphs(savedData);
+        return savedData;
+    }
+
+    @Override
+    public ProjectModel editSingleProject(String id, ProjectModel project) {
+        Optional<ProjectModel> foundProject = repository.findProjectModelById(id);
+        if (foundProject.isPresent()) {
+            project.setId(id);
+            ProjectModel savedData = saveSingleProjectSoftCascade(project);
+            sortAboutAndProductionParagraphs(savedData);
+            return savedData;
+        }
+        throw new ApiRequestException("Project with id: '" + id + "' does not exist", HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public void deleteSingleProject(String id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAllProjects() {
+        usedSoftRepository.deleteAll();
+        softRepository.deleteAll();
+        repository.deleteAll();
+    }
+
+}
