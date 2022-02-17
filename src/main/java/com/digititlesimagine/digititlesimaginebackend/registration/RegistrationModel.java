06271a6b5a01ce59ee9f69026312ca980ca50158
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-17 20:58:19 +0100

    create registration form model with JPA repository

---
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/registration/RegistrationModel.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: RegistrationModel.java
+ * Last modified: 17/02/2022, 19:16
+ * Project name: digititles-imagine-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package com.digititlesimagine.digititlesimaginebackend.registration;
+
+import com.digititlesimagine.digititlesimaginebackend.registration.registrationProperties.RegistrationPropertiesModel;
+import com.digititlesimagine.digititlesimaginebackend.utils.AuditModelWithId;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+@Data
+@Entity
+@ToString
+@Table(name = "registration")
+public class RegistrationModel extends AuditModelWithId {
+
+    @Column(name = "user_name")
+    @NotNull(message = "Blank field responsible for user name property")
+    @Size(min = 2, max = 30, message = "Field responsible for user name should have from 2 to 30 characters")
+    private String userName;
+
+    @Column(name = "user_lastname")
+    @NotNull(message = "Blank field responsible for user lastname property")
+    @Size(min = 2, max = 30, message = "Field responsible for user lastname should have from 2 to 30 characters")
+    private String userLastname;
+
+    @Column(name = "user_email")
+    @NotNull(message = "Blank field responsible for user email property")
+    @Size(min = 5, max = 30, message = "Field responsible for user lastname should have from 5 to 30 characters")
+    private String userEmail;
+
+    @Column(name = "user_message")
+    @NotNull(message = "Blank field responsible for user message property")
+    @Size(min = 10, max = 300, message = "Field responsible for user message should have from 10 to 300 characters")
+    private String userMessage;
+
+    @Valid
+    @OneToOne(targetEntity = RegistrationPropertiesModel.class, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "properties_ref_key", referencedColumnName = "primary_key")
+    @NotNull(message = "Blank object responsible for storing registration properties data")
+    private RegistrationPropertiesModel propertiesData = new RegistrationPropertiesModel();
+
+}
---
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/registration/RegistrationRepository.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: RegistrationRepository.java
+ * Last modified: 17/02/2022, 19:31
+ * Project name: digititles-imagine-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package com.digititlesimagine.digititlesimaginebackend.registration;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RegistrationRepository extends JpaRepository<RegistrationModel, String> {
+
+    @Query("SELECT r FROM RegistrationModel r WHERE r.id=:id")
+    Optional<RegistrationModel> findRegistrationModelById(@Param("id") String id);
+
+}
