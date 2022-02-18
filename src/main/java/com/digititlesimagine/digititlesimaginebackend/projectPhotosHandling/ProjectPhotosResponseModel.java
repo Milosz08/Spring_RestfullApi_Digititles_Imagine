edfdb9ef9314cfd0bed857681eeca54328c0ce74
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-02-19 00:38:12 +0100

    create photo retrieved info as JPA entity class

---
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/projectPhotosHandling/ProjectPhotosResponseModel.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl>
+ *
+ * File name: ProjectPhotoResponseEntity.java
+ * Last modified: 18/02/2022, 14:38
+ * Project name: digititles-imagine-backend
+ *
+ * Licensed under the MIT license; you may not use this file except in compliance with the License.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL
+ * COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ */
+
+package com.digititlesimagine.digititlesimaginebackend.projectPhotosHandling;
+
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@Data
+@ToString
+public class ProjectPhotosResponseModel {
+
+    private String status;
+    private List<String> fileNames;
+    private int responseStatus;
+    private long servletTime;
+
+    public ProjectPhotosResponseModel(List<String> fileNames) {
+        this.status = "Files uploaded";
+        this.fileNames = fileNames;
+        this.responseStatus = HttpStatus.CREATED.value();
+        this.servletTime = System.currentTimeMillis();
+    }
+}
