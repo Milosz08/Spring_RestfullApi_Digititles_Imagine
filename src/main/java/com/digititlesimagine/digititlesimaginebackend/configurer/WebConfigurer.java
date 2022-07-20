4e22107f25812908d03979daf84af375a6cf7401
Milosz08 <gilgamilosz451@gmail.com>
2022-07-20 22:12:53 +0200

    change configurer class

--- src/main/java/com/digititlesimagine/digititlesimaginebackend/configurer/WebConfigurer.java
+++ src/main/java/com/digititlesimagine/digititlesimaginebackend/configurer/WebConfigurer.java
@@ -19,15 +19,20 @@
 package com.digititlesimagine.digititlesimaginebackend.configurer;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-public class WebConfigurer extends WebMvcConfigurationSupport {
+public class WebConfigurer implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://digititlesimagine.com.pl");
+    }
 }
